ba3ab08a4c0530167d7fb8d23dae5f3c465e7413
yahyahujatullah1-glitch <yahyahujatullah1@gmail.com>
2025-12-14 17:40:43 +0500

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,3 @@
+npx create-next-app@latest as-op-gaming --typescript --tailwind --eslint
+# Select Yes for App Router, No for src directory (or Yes if you prefer), Yes for Import Alias
+# *Note: The code below assumes you selected 'Yes' for the `src/` directory.*
